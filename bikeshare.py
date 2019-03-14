ab0fb3ac4d5016a07360dd076975e88a9e113ac7
Mayank <mayank1.asthana@gmail.com>
2019-03-14 10:43:09 +0000

    Added docstring to the file

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#This is the doc-string for this program
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
